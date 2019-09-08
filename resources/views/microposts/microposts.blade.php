9592b4be4ac53832a14554a5844105a7adc159b9
kamekame <kamekame@gmail.com>
2019-09-08 07:58:16 +0000

    favorites

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -9,6 +9,8 @@
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
+                
+                    @include('user_favorite.favorite_button', ['microposts' => $microposts])
                 <div>
                     @if (Auth::id() == $micropost->user_id)
                         {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
